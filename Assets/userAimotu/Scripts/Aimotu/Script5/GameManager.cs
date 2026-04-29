c9513e7f4618bb6c13ed040d9c87aa572b6d8543
christina2748394 <138764863+christina2748394@users.noreply.github.com>
2026-04-29 16:22:11 -0700

    s5intro

--- Assets/userAimotu/Scripts/Aimotu/Script5/GameManager.cs
+++ Assets/userAimotu/Scripts/Aimotu/Script5/GameManager.cs
@@ -6,8 +6,13 @@ namespace S5
     public class GameManager : SceneManagerBase
     {
         public static GameManager Instance;
-        public Task_S5 taskS5;
-        public override GameObject TaskModuleObject => taskS5 != null ? taskS5.gameObject : null;
+
+        public Task_S5 taskS5_Instance;
+        public override GameObject TaskModuleObject =>
+            taskS5_Instance != null ? taskS5_Instance.gameObject : null;
+        public Task_S5 TaskS5 => taskS5_Instance;
+
+
 
         //S5 应该是要建一个新的room state？
         protected override RoomState InitialState => RoomState.Intro;
@@ -15,6 +20,7 @@ namespace S5
         {
             Instance = this;
             base.Awake();
+            Debug.Log("<color=green>[S5 GameManager]</color> 初始化完成");
         }
 
 
